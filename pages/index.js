96c13be28dfaa4fe3ee81f3eea8019ef4dd7c05d
Rebecka <rebeckagamble@hotmail.com>
2024-02-27 13:35:40 +0100

    nav

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <>
+      <Navbar />
       <p>Tja!</p>
+      
       <Link href="/admin">Go to admin page</Link>
     </>
   );
