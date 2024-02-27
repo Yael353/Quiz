ba210c1cb2178f9eea566a0a54e06f063fee2fbf
Elias-mb1 <eliasburhan0@gmail.com>
2024-02-27 13:06:55 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Chas Quiz</h1>
       <p>Tja!</p>
-      <p>hej</p>
       <Link href="/admin">Go to admin page</Link>
-    </>
+    </div>
   );
 }
