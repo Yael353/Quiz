69775eb580c249b47567878c81a3167ad120d097
Elias-mb1 <eliasburhan0@gmail.com>
2024-02-29 19:17:03 +0100

    placed stuff in center

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,28 @@ import React from "react";
 import Link from "next/link";
 import Navbar from "./components/Navbar";
 
- function Home() {
+function Home() {
   return (
     <>
       <Navbar />
-      <Link
-        href="/admin"
-        className="inline-block bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-md"
-      >
-        Admin
-      </Link>
-      <Link
-        href="/quiz"
-        className="inline-block bg-purple-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-md"
-      >
-        Guest
-      </Link>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-6">Welcome to the Quiz</h1>
+        <div className="flex space-x-4">
+          <Link
+            href="/admin"
+            className="inline-block bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-md"
+          >
+            Admin
+          </Link>
+          <Link
+            href="/quiz"
+            className="inline-block bg-purple-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-md"
+          >
+            Guest
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
-export default Home
+export default Home;
